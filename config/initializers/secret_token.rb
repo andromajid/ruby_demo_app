aaa308d4db398c1400671de88c61c4627d1be756
appleseed <andro.majid@redawning.com>
2013-10-24 15:10:07 +0700

    upload data pertama

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '0be09aaade8fdcab116417118586e24ab5502046ca1aac592cbb3125b180ac2669e0f4faa1024d277a596169bcfdcbe06a85ede6127dcfe254fba3f36104e354'
